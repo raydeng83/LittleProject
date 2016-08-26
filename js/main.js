4b123abd17fa1c36d3fe2374edd61ed4641c78c4
Le Deng <lede@Les-MacBook-Pro.local>
2016-08-26 13:45:39 -0400

    add audio symbol

--- js/main.js
+++ js/main.js
@@ -3,40 +3,51 @@ var i=0;
 $(document).ready(function() {
 	var audiot = new Audio("sound/1.mp3");
 
+	$('#volume-symbol').click(function(e) {
+		$(this).toggleClass('glyphicon-volume-up glyphicon-volume-off');
+
+		if ($(this).hasClass('glyphicon-volume-up')) {
+			audiot.play();
+		} else {
+			audiot.pause();
+		}
+	});
+
 	$('#right-arrow').click(function(){
 		if (i<messages.length-1) {
 			$('#main-text').text(messages[++i]);
 			$('#main-text').addClass('animated fadeInRightBig');
 
 			setTimeout(function() {
-     			$('#main-text').removeClass('animated fadeInRightBig');
- 			}, 1000);
+				$('#main-text').removeClass('animated fadeInRightBig');
+			}, 1000);
 
- 			$('body').css('background-image', 'url(image/'+backgroundImageUrl[i]+')');
+			$('body').css('background-image', 'url(image/'+backgroundImageUrl[i]+')');
 
-    		audiot.play();
+			// audiot.play();
 			
 		}
 	});
 
 	$('#left-arrow').click(function(){
+
 		if (i>0){
 			$('#main-text').text(messages[--i]);
 
 			$('#main-text').addClass('animated fadeInLeftBig');
 
 			setTimeout(function() {
-     			$('#main-text').removeClass('animated fadeInLeftBig');
- 			}, 1000);
+				$('#main-text').removeClass('animated fadeInLeftBig');
+			}, 1000);
 
- 			$('body').css('background-image', 'url(image/'+backgroundImageUrl[i]+')');
+			$('body').css('background-image', 'url(image/'+backgroundImageUrl[i]+')');
 
 		}
 	})
 });
 
 var messages = [
-"This is messThis is message twoThis is message twoage one",
+"This is a story between a woman and a man...",
 "This isThis is message twoThis is message two message two",
 "This isThis is message two message three",
 "This is meThis is message twoThis is message twoThis is message twoThis is message twoThis is message twoThis is message twossage four",
@@ -44,9 +55,9 @@ var messages = [
 ]
 
 var backgroundImageUrl = [
-	"1.jpg",
-	"1.jpg",
-	"2.jpg",
-	"2.jpg",
-	"1.jpg"
+"",
+"1.jpg",
+"2.jpg",
+"",
+""
 ]
